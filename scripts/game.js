c4a8ef1d83ec3118915ed69aa31cb1a2a44558c6
Jesús Lorenzo Limón <132434631+Zarritas@users.noreply.github.com>
2024-09-05 13:29:32 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -21,7 +21,7 @@ let config = {
         default: 'arcade',
         arcade: {
             gravity: { y: nivelGravedad },
-            debug: true
+            debug: false
         }
     },
     scene: {
